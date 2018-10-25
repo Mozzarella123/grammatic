4fd434f2368aff99425d998135a42dc0e240c8dd
slon <serafim.gerasimov@mail.ru>
2018-10-25 22:06:30 +0300

    Automat for expression checked

---
+++ src/main/java/Automat/AutomatConfig.java
@@ -0,0 +1,17 @@
+package Automat;
+
+import javax.xml.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlType(namespace = "au")
+@XmlRootElement(namespace = "au")
+public class AutomatConfig {
+
+    @XmlElement(type = AutomatState.class,namespace = "au")
+    public List<AutomatState> states = new ArrayList<>();
+
+    @XmlIDREF
+    @XmlAttribute(required = true,name = "start",namespace = "au")
+    public AutomatState startState;
+}
